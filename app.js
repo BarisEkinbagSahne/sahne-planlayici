0bbdee49b5f6c0d11be6867b6521ecb2f0be1156
barisekinbag <barisekinbag@gmail.com>
2026-06-02 10:02:53 +0300

    Gorunum 2 geri getir ve gecis butonlarini ac.

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const state = {
   roadCache: {},
   returnToIzmirDraft: false,
   teamFilter: "ALL",
-  eventView: "table",
+  eventView: "grid",
   gridStartDate: null,
   gridEndDate: null,
   gridDidInitialScroll: false,
